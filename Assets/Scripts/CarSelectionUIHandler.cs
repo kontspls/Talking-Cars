a1aaab65c3e85f9cb8e62374e9b7a7200bc9feb9
Konstantinos Tassopoulos <tassopouloskon@gmail.com>
2025-05-06 11:15:14 +0200

    Car Selection Functionality Added
     - Added functionality to select random cars based on characteristics in the car selection screen

---
+++ Assets/Scripts/CarSelectionUIHandler.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+
+public class CarSelectionUIHandler : MonoBehaviour
+{
+    [SerializeField] private Button fatButton;
+    [SerializeField] private Button intelligentButton;
+    [SerializeField] private Button kindButton;
+
+    public List<GameObject> cars;
+    private int selectedIndex;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        cars = MainManager.Instance.cars;
+
+        fatButton.onClick.AddListener(() => SelectCar(0, 2));
+        intelligentButton.onClick.AddListener(() => SelectCar(2, 3));
+        kindButton.onClick.AddListener(() => SelectCar(3, 5));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (MainManager.Instance.SelectedCar != null)
+        {
+            StartGame();
+        }
+    }
+    private void SelectCar(int minInclusive, int maxExclusive)
+    {
+        selectedIndex = Random.Range(minInclusive, maxExclusive);
+        MainManager.Instance.SelectedCar = cars[selectedIndex];
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
+        
---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class MainManager : MonoBehaviour
+{
+    // Start() and Update() methods deleted - we don't need them right now
+
+    public static MainManager Instance { get; private set; }
+    public GameObject SelectedCar;
+    public List<GameObject> cars;
+
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+
+    }
+}
